3adf00a492aacdd227d6d4a157599a686b3d4c75
Vara Prasad <vprasad@equalexperts.com>
2018-03-24 11:20:06 +0530

    Add Empty, Plain.

--- src/main/java/com/varaprasadps/image/EmptyGenerator.java
+++ src/main/java/com/varaprasadps/image/EmptyGenerator.java
@@ -7,34 +7,20 @@ import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class AnniGenerator {
+public class EmptyGenerator {
 
     public static void main(final String[] args) throws IOException {
         int x = 1824;
         int y = 16;
         BufferedImage img = map(x, y);
-        saveBMP(img, String.format("z-data/out/anni-%s-%s.bmp", x, y));
+        saveBMP(img, String.format("z-data/out/empty-%s-%s.bmp", x, y));
     }
 
     private static BufferedImage map(int sizeX, int sizeY) {
         final BufferedImage res = new BufferedImage(sizeX, sizeY, BufferedImage.TYPE_INT_RGB);
-        Color start;
-        Color other;
         for (int x = 0; x < sizeY; x++) {
-            if (x % 2 == 0) {
-                start = Color.WHITE;
-                other = Color.BLACK;
-            } else {
-                start = Color.BLACK;
-                other = Color.WHITE;
-            }
             for (int y = 0; y < sizeX; y++) {
-                if (y % 2 == 0) {
-                    res.setRGB(y, x, start.getRGB());
-                } else {
-                    res.setRGB(y, x, other.getRGB());
-                }
-
+                res.setRGB(y, x, Color.WHITE.getRGB());
             }
         }
         return res;
--- src/main/java/com/varaprasadps/image/PlainGenerator.java
+++ src/main/java/com/varaprasadps/image/PlainGenerator.java
@@ -7,21 +7,34 @@ import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class KaliGenerator {
-
+public class PlainGenerator {
 
     public static void main(final String[] args) throws IOException {
         int x = 1824;
         int y = 16;
         BufferedImage img = map(x, y);
-        saveBMP(img, String.format("z-data/out/kali-%s-%s.bmp", x, y));
+        saveBMP(img, String.format("z-data/out/plain-%s-%s.bmp", x, y));
     }
 
     private static BufferedImage map(int sizeX, int sizeY) {
         final BufferedImage res = new BufferedImage(sizeX, sizeY, BufferedImage.TYPE_INT_RGB);
+        Color start;
+        Color other;
         for (int x = 0; x < sizeY; x++) {
+            if (x % 2 == 0) {
+                start = Color.WHITE;
+                other = Color.BLACK;
+            } else {
+                start = Color.BLACK;
+                other = Color.WHITE;
+            }
             for (int y = 0; y < sizeX; y++) {
-                res.setRGB(y, x, Color.WHITE.getRGB());
+                if (y % 2 == 0) {
+                    res.setRGB(y, x, start.getRGB());
+                } else {
+                    res.setRGB(y, x, other.getRGB());
+                }
+
             }
         }
         return res;
