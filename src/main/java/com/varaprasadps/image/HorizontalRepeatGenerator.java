06934c11e00964aed1eb9d232271f064cc605d4e
Vara Prasad <vprasad@equalexperts.com>
2018-03-24 14:03:20 +0530

    Add Horizontal Repeat Generator.

---
+++ src/main/java/com/varaprasadps/image/HorizontalRepeatGenerator.java
@@ -0,0 +1,48 @@
+package com.varaprasadps.image;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class HorizontalRepeatGenerator {
+
+
+    public static void main(final String[] args) throws IOException {
+
+        String out = "z-data/out/horizontal-layout-%s-%s.bmp";
+        int times = 5;
+
+        String path = "z-data/out/plain-10-50.bmp";
+        BufferedImage image = ImageIO.read(new File(path));
+
+        int width = image.getWidth() * times;
+        int height = image.getHeight();
+
+        BufferedImage bi = map(width, height, image);
+        displayPixels(bi);
+        saveBMP(bi, String.format(out, width, height));
+    }
+
+    private static void displayPixels(BufferedImage fileOne) {
+        System.out.println(String.format("Width : %s, Height : %s", fileOne.getWidth(), fileOne.getHeight()));
+    }
+
+    private static BufferedImage map(int sizeX, int sizeY, BufferedImage input) {
+        final BufferedImage result = new BufferedImage(sizeX, sizeY, BufferedImage.TYPE_INT_RGB);
+        int inputWidth = input.getWidth();
+
+        for (int x = 0; x < sizeX; x++) {
+            for (int y = 0; y < sizeY; y++) {
+                result.setRGB(x, y, input.getRGB(x % inputWidth, y));
+            }
+        }
+        return result;
+    }
+
+    private static void saveBMP(final BufferedImage bi, final String path) throws IOException {
+        ImageIO.write(bi, "bmp", new File(path));
+    }
+
+
+}
