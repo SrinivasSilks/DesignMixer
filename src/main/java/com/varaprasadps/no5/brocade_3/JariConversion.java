55e191ad099df309bed683fb5b8dbaa8cd1b4c58
Vara Prasad <vprasad@equalexperts.com>
2018-06-12 18:10:27 +0530

    no 5 brocade 3

---
+++ src/main/java/com/varaprasadps/no5/brocade_3/JariConversion.java
@@ -0,0 +1,49 @@
+package com.varaprasadps.no5.brocade_3;
+
+import com.varaprasadps.image.AddLayoutGenerator;
+import com.varaprasadps.image.EmptyGenerator;
+import com.varaprasadps.image.ReverseGenerator;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class JariConversion {
+
+    public static void main(final String[] args) throws IOException {
+
+        String out = "z-data/out/5/3/jari-%s-%s.bmp";
+        final BufferedImage jari = ImageIO.read(new File("z-data/in/5/3/BROCADE_JARI.bmp"));
+
+        List<BufferedImage> inputBIs = new LinkedList<>();
+        inputBIs.add(EmptyGenerator.get(jari.getWidth(), 32));
+        inputBIs.add(EmptyGenerator.get(jari.getWidth(), 4));
+        inputBIs.add(EmptyGenerator.get(jari.getWidth(), 12));
+        inputBIs.add(jari);
+        inputBIs.add(EmptyGenerator.get(jari.getWidth(), 16));
+
+        int repeatWidth = 0;
+        int repeatHeight = 0;
+
+        for (BufferedImage bi : inputBIs) {
+            displayPixels(bi);
+            repeatWidth = bi.getWidth();
+            repeatHeight += bi.getHeight();
+        }
+        BufferedImage bi = ReverseGenerator.get(AddLayoutGenerator.get(repeatWidth, repeatHeight, inputBIs));
+        displayPixels(bi);
+        saveBMP(bi, String.format(out, repeatWidth, repeatHeight));
+    }
+
+    private static void displayPixels(BufferedImage fileOne) {
+        System.out.println(String.format("Width : %s, Height : %s", fileOne.getWidth(), fileOne.getHeight()));
+    }
+
+    private static void saveBMP(final BufferedImage bi, final String path) throws IOException {
+        ImageIO.write(bi, "bmp", new File(path));
+    }
+
+}
