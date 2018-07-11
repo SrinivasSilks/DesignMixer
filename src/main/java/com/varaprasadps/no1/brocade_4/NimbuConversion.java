62757367d47958f94037f9b9c220b71582354908
Vara Prasad <vprasad@equalexperts.com>
2018-07-11 17:02:43 +0530

    brocade nimbu complete

--- src/main/java/com/varaprasadps/no1/brocade_4/NimbuConversion.java
+++ src/main/java/com/varaprasadps/no1/brocade_4/NimbuConversion.java
@@ -1,9 +1,6 @@
 package com.varaprasadps.no1.brocade_4;
 
-import com.varaprasadps.image.AddLayoutGenerator;
-import com.varaprasadps.image.EmptyGenerator;
-import com.varaprasadps.image.HorizontalRepeatGenerator;
-import com.varaprasadps.image.ReverseGenerator;
+import com.varaprasadps.image.*;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -17,10 +14,31 @@ public class NimbuConversion {
     public static void main(final String[] args) throws IOException {
 
         String out = "z-data/out/1/4_nimbu-%s-%s.bmp";
+        final BufferedImage skirt = ImageIO.read(new File("z-data/in/1/SKIRT_NIMBU.bmp"));
 
         List<BufferedImage> inputBIs = new LinkedList<>();
+
+        // Box
+        inputBIs.add(ReverseGenerator.get(EmptyGenerator.get(skirt.getWidth(), 2)));
+        inputBIs.add(EmptyGenerator.get(skirt.getWidth(), 2));
+        // Khali
+        inputBIs.add(EmptyGenerator.get(skirt.getWidth(), 4));
+        //Achu
+        inputBIs.add(EmptyGenerator.get(skirt.getWidth(), 8));
+
+        // Locking
+        inputBIs.add(ReverseGenerator.get(PlainGenerator.get(skirt.getWidth(), 20)));
+
         inputBIs.add(HorizontalRepeatGenerator.get(12, ImageIO.read(new File("z-data/in/1/4NIMBU.bmp"))));
-        inputBIs.add(ImageIO.read(new File("z-data/in/1/SKIRT_NIMBU.bmp")));
+        inputBIs.add(skirt);
+
+        // Jari
+        inputBIs.add(EmptyGenerator.get(skirt.getWidth(), 60));
+
+        //Khali
+        inputBIs.add(EmptyGenerator.get(skirt.getWidth(), 8));
+        // Achu
+        inputBIs.add(EmptyGenerator.get(skirt.getWidth(), 8));
 
 
         int repeatWidth = 0;
