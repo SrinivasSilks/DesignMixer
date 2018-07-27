91fb90998eb977a003dba429185d27e1916e00ae
Vara Prasad <vprasad@equalexperts.com>
2018-07-27 09:29:14 +0530

    chandra 2 brocade

---
+++ src/main/java/com/varaprasadps/chandra/no2/brocade/NimbuConversion.java
@@ -0,0 +1,57 @@
+package com.varaprasadps.chandra.no2.brocade;
+
+import com.varaprasadps.image.*;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class NimbuConversion {
+
+    public static void main(final String[] args) throws IOException {
+
+        String out = "z-chandra/out/2/nimbu-%s-%s.bmp";
+        final BufferedImage skirt = ImageIO.read(new File("z-chandra/in/2/B_NIMBU.bmp"));
+
+        List<BufferedImage> inputBIs = new LinkedList<>();
+
+        // Box
+        inputBIs.add(ReverseGenerator.get(EmptyGenerator.get(skirt.getWidth(), 2)));
+        inputBIs.add(EmptyGenerator.get(skirt.getWidth(), 2));
+        //Achu
+        inputBIs.add(EmptyGenerator.get(skirt.getWidth(), 12));
+
+        inputBIs.add(skirt);
+
+        // Box
+        inputBIs.add(ReverseGenerator.get(EmptyGenerator.get(skirt.getWidth(), 2)));
+        inputBIs.add(EmptyGenerator.get(skirt.getWidth(), 2));
+        // Achu
+        inputBIs.add(EmptyGenerator.get(skirt.getWidth(), 12));
+
+
+        int repeatWidth = 0;
+        int repeatHeight = 0;
+
+        for (BufferedImage bi : inputBIs) {
+            displayPixels(bi);
+            repeatWidth = bi.getWidth();
+            repeatHeight += bi.getHeight();
+        }
+        BufferedImage bi = AddLayoutGenerator.get(repeatWidth, repeatHeight, inputBIs);
+        displayPixels(bi);
+        saveBMP(bi, String.format(out, repeatWidth, repeatHeight));
+    }
+
+    private static void displayPixels(BufferedImage fileOne) {
+        System.out.println(String.format("Width : %s, Height : %s", fileOne.getWidth(), fileOne.getHeight()));
+    }
+
+    private static void saveBMP(final BufferedImage bi, final String path) throws IOException {
+        ImageIO.write(bi, "bmp", new File(path));
+    }
+
+}
