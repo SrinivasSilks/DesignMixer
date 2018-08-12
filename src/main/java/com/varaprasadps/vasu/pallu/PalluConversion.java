e2493c3b1b678da1bc4e437e50b42b62fae79259
Vara Prasad <vprasad@equalexperts.com>
2018-08-12 21:06:29 +0530

    vasu no 4 pallu

---
+++ src/main/java/com/varaprasadps/vasu/pallu/PalluConversion.java
@@ -0,0 +1,44 @@
+package com.varaprasadps.vasu.pallu;
+
+import com.varaprasadps.image.ColumnRepeatGenerator;
+import com.varaprasadps.image.HorizontalFlipGenerator;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class PalluConversion {
+
+    public static void main(final String[] args) throws IOException {
+        JariConversion.main(null);
+        RaniConversion.main(null);
+        String out = "z-vasu/out/4/pallu-%s-%s.bmp";
+
+        List<String> inputs = new LinkedList<>();
+        inputs.add("z-vasu/out/4/p-rani-2040-1536.bmp");
+        inputs.add("z-vasu/out/4/p-jari-2040-1536.bmp");
+
+        List<BufferedImage> inputBIs = new LinkedList<>();
+        for (String input : inputs) {
+            inputBIs.add(ImageIO.read(new File(input)));
+        }
+
+        BufferedImage bi = HorizontalFlipGenerator.get(ColumnRepeatGenerator.get(inputBIs));
+        displayPixels(bi);
+        saveBMP(bi, String.format(out, bi.getWidth(), bi.getHeight()));
+    }
+
+
+    private static void displayPixels(BufferedImage fileOne) {
+        System.out.println(String.format("Width : %s, Height : %s", fileOne.getWidth(), fileOne.getHeight()));
+    }
+
+    private static void saveBMP(final BufferedImage bi, final String path) throws IOException {
+        ImageIO.write(bi, "bmp", new File(path));
+    }
+
+
+}
---
+++ src/main/java/com/varaprasadps/vasu/pallu/RaniConversion.java
@@ -0,0 +1,64 @@
+package com.varaprasadps.vasu.pallu;
+
+import com.varaprasadps.image.*;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class RaniConversion {
+
+    public static void main(final String[] args) throws IOException {
+
+        String out = "z-vasu/out/4/p-rani-%s-%s.bmp";
+
+        final BufferedImage pallu = HorizontalFlipGenerator.get(ImageIO.read(new File("z-vasu/in/4/PALLU_RANI.bmp")));
+        List<BufferedImage> inputBIs = new LinkedList<>();
+
+        // Box
+        inputBIs.add(EmptyGenerator.get(pallu.getWidth(), 2));
+        inputBIs.add(ReverseGenerator.get(EmptyGenerator.get(pallu.getWidth(), 2)));
+
+        //Achu
+        inputBIs.add(AchuLayoutGenerator.get(pallu.getWidth(), 12));
+
+        inputBIs.add(pallu);
+
+        // Locking
+        inputBIs.add(PlainGenerator.get(pallu.getWidth(), 16));
+
+        // Box
+        inputBIs.add(EmptyGenerator.get(pallu.getWidth(), 2));
+        inputBIs.add(ReverseGenerator.get(EmptyGenerator.get(pallu.getWidth(), 2)));
+
+        // Achu
+        inputBIs.add(AchuLayoutGenerator.get(pallu.getWidth(), 12));
+
+        // Board Khali
+        inputBIs.add(EmptyGenerator.get(pallu.getWidth(), 128));
+
+        int repeatWidth = 0;
+        int repeatHeight = 0;
+
+        for (BufferedImage bi : inputBIs) {
+            displayPixels(bi);
+            repeatWidth = bi.getWidth();
+            repeatHeight += bi.getHeight();
+        }
+        BufferedImage bi = AddLayoutGenerator.get(repeatWidth, repeatHeight, inputBIs);
+        displayPixels(bi);
+        saveBMP(bi, String.format(out, repeatWidth, repeatHeight));
+    }
+
+    private static void displayPixels(BufferedImage fileOne) {
+        System.out.println(String.format("Width : %s, Height : %s", fileOne.getWidth(), fileOne.getHeight()));
+    }
+
+    private static void saveBMP(final BufferedImage bi, final String path) throws IOException {
+        ImageIO.write(bi, "bmp", new File(path));
+    }
+
+}
