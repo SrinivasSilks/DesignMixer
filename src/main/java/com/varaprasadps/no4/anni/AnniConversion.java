16756eee1e1d3154fcf31f270386a8e2804179d0
Vara Prasad <vprasad@equalexperts.com>
2018-09-02 21:43:55 +0530

    anni no 4

---
+++ src/main/java/com/varaprasadps/no4/anni/AnniConversion.java
@@ -0,0 +1,60 @@
+package com.varaprasadps.no4.anni;
+
+import com.varaprasadps.image.*;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class AnniConversion {
+
+    public static void main(final String[] args) throws IOException {
+
+        String out = "z-data/out/4/anni-%s-%s.bmp";
+        final BufferedImage border = ImageIO.read(new File("z-data/in/4/BORDER.bmp"));
+        final int width = border.getWidth();
+
+        List<BufferedImage> inputBIs = new LinkedList<>();
+
+        inputBIs.add(EmptyGenerator.get(width, 32));
+
+        // locking
+        inputBIs.add(PlainGenerator.get(width, 4));
+        //achu
+        inputBIs.add(AchuLayoutGenerator.get(width, 12));
+
+        // Skirt and Allover
+        inputBIs.add(PlainGenerator.get(width, 1200));
+        inputBIs.add(border);
+
+        // Box
+        inputBIs.add(ReverseGenerator.get(EmptyGenerator.get(width, 2)));
+        inputBIs.add(EmptyGenerator.get(width, 2));
+        // Achu
+        inputBIs.add(AchuLayoutGenerator.get(width, 12));
+
+        int repeatWidth = 0;
+        int repeatHeight = 0;
+
+        for (BufferedImage bi : inputBIs) {
+            displayPixels(bi);
+            repeatWidth = bi.getWidth();
+            repeatHeight += bi.getHeight();
+        }
+        BufferedImage bi = ReverseGenerator.get(AddLayoutGenerator.get(repeatWidth, repeatHeight, inputBIs));
+        displayPixels(bi);
+        saveBMP(bi, String.format(out, repeatWidth, repeatHeight));
+    }
+
+    private static void displayPixels(BufferedImage fileOne) {
+        System.out.println(String.format("Width : %s, Height : %s", fileOne.getWidth(), fileOne.getHeight()));
+    }
+
+    private static void saveBMP(final BufferedImage bi, final String path) throws IOException {
+        ImageIO.write(bi, "bmp", new File(path));
+    }
+
+}
