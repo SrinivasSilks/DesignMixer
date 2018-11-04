e2c1463fd60279a3652d956430b9f04698138bae
Vara Prasad <vprasad@equalexperts.com>
2018-11-04 14:55:11 +0530

    No 4 pallu

---
+++ src/main/java/com/varaprasadps/no5/pallu4/RaniConversion.java
@@ -0,0 +1,49 @@
+package com.varaprasadps.no5.pallu4;
+
+import com.varaprasadps.image.AddLayoutGenerator;
+import com.varaprasadps.image.EmptyGenerator;
+import com.varaprasadps.image.ReverseGenerator;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class RaniConversion {
+
+    public static void main(final String[] args) throws IOException {
+
+        String out = "z-data/out/5/4p-rani-%s-%s.bmp";
+
+        List<BufferedImage> inputBIs = new LinkedList<>();
+        inputBIs.add(EmptyGenerator.get(1920, 32));
+        inputBIs.add(EmptyGenerator.get(1920, 2));
+        inputBIs.add(ReverseGenerator.get(EmptyGenerator.get(1200, 2)));
+        inputBIs.add(EmptyGenerator.get(1920, 12));
+        inputBIs.add(ImageIO.read(new File("z-data/in/5/variety/PALLU_RANI.bmp")));
+        inputBIs.add(EmptyGenerator.get(1920, 16));
+
+        int repeatWidth = 0;
+        int repeatHeight = 0;
+
+        for (BufferedImage bi : inputBIs) {
+            displayPixels(bi);
+            repeatWidth = bi.getWidth();
+            repeatHeight += bi.getHeight();
+        }
+        BufferedImage bi = ReverseGenerator.get(AddLayoutGenerator.get(repeatWidth, repeatHeight, inputBIs));
+        displayPixels(bi);
+        saveBMP(bi, String.format(out, repeatWidth, repeatHeight));
+    }
+
+    private static void displayPixels(BufferedImage fileOne) {
+        System.out.println(String.format("Width : %s, Height : %s", fileOne.getWidth(), fileOne.getHeight()));
+    }
+
+    private static void saveBMP(final BufferedImage bi, final String path) throws IOException {
+        ImageIO.write(bi, "bmp", new File(path));
+    }
+
+}
