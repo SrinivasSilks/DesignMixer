841a2bba9610d2f37ddcf831191a164f5067f0ef
Vara Prasad <vprasad@equalexperts.com>
2019-04-01 07:49:11 +0530

    no 1 design 9

---
+++ src/main/java/com/varaprasadps/no1/brocade_9/BrocadeConversion.java
@@ -0,0 +1,48 @@
+package com.varaprasadps.no1.brocade_9;
+
+import com.varaprasadps.image.ColumnRepeatGenerator;
+import com.varaprasadps.image.HorizontalFlipGenerator;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class BrocadeConversion {
+
+    public static void main(final String[] args) throws IOException {
+
+        JariConversion.main(null);
+        NimbuConversion.main(null);
+        RaniConversion.main(null);
+
+        String out = "z-data/out/1/9_1brocade-%s-%s.bmp";
+
+        List<String> inputs = new LinkedList<>();
+        inputs.add("z-data/out/1/9_rani-600-1792.bmp");
+        inputs.add("z-data/out/1/9_jari-600-1792.bmp");
+        inputs.add("z-data/out/1/9_nimbu-600-1792.bmp");
+
+        List<BufferedImage> inputBIs = new LinkedList<>();
+        for (String input : inputs) {
+            inputBIs.add(ImageIO.read(new File(input)));
+        }
+
+        BufferedImage bi = HorizontalFlipGenerator.get(ColumnRepeatGenerator.get(inputBIs));
+        displayPixels(bi);
+        saveBMP(bi, String.format(out, bi.getWidth(), bi.getHeight()));
+    }
+
+
+    private static void displayPixels(BufferedImage fileOne) {
+        System.out.println(String.format("Width : %s, Height : %s", fileOne.getWidth(), fileOne.getHeight()));
+    }
+
+    private static void saveBMP(final BufferedImage bi, final String path) throws IOException {
+        ImageIO.write(bi, "bmp", new File(path));
+    }
+
+
+}
---
+++ src/main/java/com/varaprasadps/no1/brocade_9/JariConversion.java
@@ -0,0 +1,66 @@
+package com.varaprasadps.no1.brocade_9;
+
+import com.varaprasadps.image.*;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class JariConversion {
+
+    public static void main(final String[] args) throws IOException {
+
+        String out = "z-data/out/1/9_jari-%s-%s.bmp";
+        final BufferedImage skirt = HorizontalFlipGenerator.get(ImageIO.read(new File("z-data/in/1/9/JARI.bmp")));
+
+        List<BufferedImage> inputBIs = new LinkedList<>();
+        // Box
+        inputBIs.add(EmptyGenerator.get(skirt.getWidth(), 2));
+        inputBIs.add(EmptyGenerator.get(skirt.getWidth(), 2));
+        // Khali
+        inputBIs.add(EmptyGenerator.get(skirt.getWidth(), 4));
+        //Achu
+        inputBIs.add(EmptyGenerator.get(skirt.getWidth(), 8));
+
+        // Locking
+        inputBIs.add(HorizontalFlipGenerator.get(ReverseGenerator.get(StepLayoutGenerator.get(skirt.getWidth(), 5))));
+
+        inputBIs.add(skirt);
+
+        // Jari
+        inputBIs.add(EmptyGenerator.get(skirt.getWidth(), 60));
+
+        // Box
+        inputBIs.add(EmptyGenerator.get(skirt.getWidth(), 2));
+        inputBIs.add(EmptyGenerator.get(skirt.getWidth(), 2));
+        // Khali
+        inputBIs.add(EmptyGenerator.get(skirt.getWidth(), 4));
+
+        // Achu
+        inputBIs.add(EmptyGenerator.get(skirt.getWidth(), 8));
+
+        int repeatWidth = 0;
+        int repeatHeight = 0;
+
+        for (BufferedImage bi : inputBIs) {
+            displayPixels(bi);
+            repeatWidth = bi.getWidth();
+            repeatHeight += bi.getHeight();
+        }
+        BufferedImage bi = AddLayoutGenerator.get(repeatWidth, repeatHeight, inputBIs);
+        displayPixels(bi);
+        saveBMP(bi, String.format(out, repeatWidth, repeatHeight));
+    }
+
+    private static void displayPixels(BufferedImage fileOne) {
+        System.out.println(String.format("Width : %s, Height : %s", fileOne.getWidth(), fileOne.getHeight()));
+    }
+
+    private static void saveBMP(final BufferedImage bi, final String path) throws IOException {
+        ImageIO.write(bi, "bmp", new File(path));
+    }
+
+}
