02044ba6dfde931105b6beff63cc46eda4e7fec3
varaprasad <poppu.vara@gmail.com>
2019-12-04 10:27:13 +0530

    no 2 plain

---
+++ src/main/java/com/varaprasadps/no2/loom/AnniConversion.java
@@ -0,0 +1,46 @@
+package com.varaprasadps.no2.loom;
+
+import com.varaprasadps.image.AddLayoutGenerator;
+import com.varaprasadps.image.HorizontalFlipGenerator;
+import com.varaprasadps.image.PlainGenerator;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class AnniConversion {
+
+    public static void main(final String[] args) throws IOException {
+
+        String out = "z-data/out/2/2688-ANNI.bmp";
+
+        List<BufferedImage> inputBIs = new LinkedList<>();
+
+        // Achu
+        inputBIs.add(PlainGenerator.get(2, 2688));
+
+        int repeatWidth = 0;
+        int repeatHeight = 0;
+
+        for (BufferedImage bi : inputBIs) {
+            displayPixels(bi);
+            repeatWidth = bi.getWidth();
+            repeatHeight += bi.getHeight();
+        }
+        BufferedImage bi = HorizontalFlipGenerator.get(AddLayoutGenerator.get(repeatWidth, repeatHeight, inputBIs));
+        displayPixels(bi);
+        saveBMP(bi, String.format(out, repeatWidth, repeatHeight));
+    }
+
+    private static void displayPixels(BufferedImage fileOne) {
+        System.out.println(String.format("Width : %s, Height : %s", fileOne.getWidth(), fileOne.getHeight()));
+    }
+
+    private static void saveBMP(final BufferedImage bi, final String path) throws IOException {
+        ImageIO.write(bi, "bmp", new File(path));
+    }
+
+}
