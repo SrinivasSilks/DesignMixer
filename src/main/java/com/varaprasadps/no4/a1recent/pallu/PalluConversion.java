8d9566f3254106a35098ee29a4532f86f526390b
varaprasad <poppu.vara@gmail.com>
2020-01-09 09:39:02 +0530

    no 4 new design

---
+++ src/main/java/com/varaprasadps/no4/a1recent/pallu/PalluConversion.java
@@ -0,0 +1,43 @@
+package com.varaprasadps.no4.a1recent.pallu;
+
+import com.varaprasadps.image.ColumnRepeatGenerator;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class PalluConversion {
+
+    public static void main(final String[] args) throws IOException {
+        JariConversion.main(null);
+        RaniConversion.main(null);
+        String out = "z-data/out/4/a1recent/pallu-%s-%s.bmp";
+
+        List<String> inputs = new LinkedList<>();
+        inputs.add("z-data/out/4/a1recent/p-rani-1800-1440.bmp");
+        inputs.add("z-data/out/4/a1recent/p-jari-1800-1440.bmp");
+
+        List<BufferedImage> inputBIs = new LinkedList<>();
+        for (String input : inputs) {
+            inputBIs.add(ImageIO.read(new File(input)));
+        }
+
+        BufferedImage bi = ColumnRepeatGenerator.get(inputBIs);
+        displayPixels(bi);
+        saveBMP(bi, String.format(out, bi.getWidth(), bi.getHeight()));
+    }
+
+
+    private static void displayPixels(BufferedImage fileOne) {
+        System.out.println(String.format("Width : %s, Height : %s", fileOne.getWidth(), fileOne.getHeight()));
+    }
+
+    private static void saveBMP(final BufferedImage bi, final String path) throws IOException {
+        ImageIO.write(bi, "bmp", new File(path));
+    }
+
+
+}
