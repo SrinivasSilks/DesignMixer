0bbb1fbf39a617a619fda8394502bad62e2cb362
varaprasad <poppu.vara@gmail.com>
2020-03-17 14:45:22 +0530

    border number 1

---
+++ src/main/java/com/varaprasadps/no1/BorderGenerator.java
@@ -0,0 +1,53 @@
+package com.varaprasadps.no1;
+
+import com.varaprasadps.image.AddLayoutGenerator;
+import com.varaprasadps.image.CutLayoutGenerator;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class BorderGenerator {
+
+    public static void main(final String[] args) throws IOException {
+        String out = "z-data/in/1/a2recent/border.bmp";
+
+        BufferedImage right = ImageIO.read(new File("z-data/in/1/a2recent/41.bmp"));
+
+        List<BufferedImage> inputBIs = new LinkedList<>();
+
+        List<BufferedImage> images = CutLayoutGenerator.get(right, 32);
+        BufferedImage A = images.get(0);
+
+        inputBIs.add(A);
+
+        images = CutLayoutGenerator.get(images.get(1), 312);
+        inputBIs.add(images.get(1));
+        inputBIs.add(images.get(0));
+
+        int repeatWidth = 0;
+        int repeatHeight = 0;
+
+        for (BufferedImage bi : inputBIs) {
+            displayPixels(bi);
+            repeatWidth = bi.getWidth();
+            repeatHeight += bi.getHeight();
+        }
+
+        BufferedImage bi = AddLayoutGenerator.get(repeatWidth, repeatHeight, inputBIs);
+        displayPixels(bi);
+        saveBMP(bi, String.format(out, repeatWidth, repeatHeight));
+    }
+
+    private static void displayPixels(BufferedImage fileOne) {
+        System.out.println(String.format("Width : %s, Height : %s", fileOne.getWidth(), fileOne.getHeight()));
+    }
+
+    static void saveBMP(final BufferedImage bi, final String path) throws IOException {
+        ImageIO.write(bi, "bmp", new File(path));
+    }
+
+}
