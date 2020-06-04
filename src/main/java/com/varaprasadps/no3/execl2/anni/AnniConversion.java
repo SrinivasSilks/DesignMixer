4d4c942d03f44eaaf9f097ed26b2e29d92a2574c
varaprasad <vara.prasad@xinja.com.au>
2020-06-04 23:54:45 +0530

    no 3 excel2

---
+++ src/main/java/com/varaprasadps/no3/execl2/anni/AnniConversion.java
@@ -0,0 +1,81 @@
+package com.varaprasadps.no3.execl2.anni;
+
+import com.varaprasadps.image.*;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class AnniConversion {
+
+    public static void main(final String[] args) throws IOException {
+
+        String out = "z-data/out/3/excel2/anni-%s-%s.bmp";
+
+        final BufferedImage left = VerticalFlipGenerator.get(ImageIO.read(new File("z-data/in/3/excel2/border1.bmp")));
+        final BufferedImage right = ImageIO.read(new File("z-data/in/3/excel2/border1.bmp"));
+
+        final int width = left.getWidth();
+
+        List<BufferedImage> inputBIs = new LinkedList<>();
+
+        inputBIs.add(EmptyGenerator.get(width, 32));
+
+        //box
+        inputBIs.add(ReverseGenerator.get(EmptyGenerator.get(width, 2)));
+        inputBIs.add(EmptyGenerator.get(width, 2));
+        //mispick
+        inputBIs.add(CutLayoutGenerator.get(AchuLayoutGenerator.get(width, 4), 2).get(0));
+        //kali
+        inputBIs.add(EmptyGenerator.get(width, 2));
+        //achu
+        inputBIs.add(AchuLayoutGenerator.get(width, 8));
+
+
+        //border
+        inputBIs.add(left);
+        // locking
+        inputBIs.add(PlainGenerator.get(width, 20));
+        //body
+        inputBIs.add(PlainGenerator.get(width, 600));
+        // locking
+        inputBIs.add(PlainGenerator.get(width, 20));
+        //border
+        inputBIs.add(right);
+
+
+        //box
+        inputBIs.add(ReverseGenerator.get(EmptyGenerator.get(width, 2)));
+        inputBIs.add(EmptyGenerator.get(width, 2));
+        //mispick
+        inputBIs.add(ReverseGenerator.get(CutLayoutGenerator.get(AchuLayoutGenerator.get(width, 4), 2).get(0)));
+        //kali
+        inputBIs.add(EmptyGenerator.get(width, 2));
+        // Achu
+        inputBIs.add(AchuLayoutGenerator.get(width, 8));
+
+        int repeatWidth = 0;
+        int repeatHeight = 0;
+
+        for (BufferedImage bi : inputBIs) {
+            displayPixels(bi);
+            repeatWidth = bi.getWidth();
+            repeatHeight += bi.getHeight();
+        }
+        BufferedImage bi = ReverseGenerator.get(AddLayoutGenerator.get(repeatWidth, repeatHeight, inputBIs));
+        displayPixels(bi);
+        saveBMP(bi, String.format(out, repeatWidth, repeatHeight));
+    }
+
+    private static void displayPixels(BufferedImage fileOne) {
+        System.out.println(String.format("Width : %s, Height : %s", fileOne.getWidth(), fileOne.getHeight()));
+    }
+
+    private static void saveBMP(final BufferedImage bi, final String path) throws IOException {
+        ImageIO.write(bi, "bmp", new File(path));
+    }
+
+}
