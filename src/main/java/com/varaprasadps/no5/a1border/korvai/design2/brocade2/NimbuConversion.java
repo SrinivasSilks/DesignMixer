c88494b3b6b2fbc0f960db0c392bfeb0345b466f
“varaprasadps-xinja“ <vara.prasad@xinja.com.au>
2020-06-30 08:36:37 +0530

    Loom 5 broacade 2

---
+++ src/main/java/com/varaprasadps/no5/a1border/korvai/design2/brocade2/NimbuConversion.java
@@ -0,0 +1,61 @@
+package com.varaprasadps.no5.a1border.korvai.design2.brocade2;
+
+import com.varaprasadps.image.*;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class NimbuConversion {
+
+    public static void main(final String[] args) throws IOException {
+
+        String out = "z-data/out/5/a1recent/korvai/design2/2nimbu-%s-%s.bmp";
+        final BufferedImage brocade = PlainGenerator.get(280 , 800);
+
+        List<BufferedImage> inputBIs = new LinkedList<>();
+        int width = brocade.getWidth();
+
+        inputBIs.add(EmptyGenerator.get(width, 32));
+        //box
+        inputBIs.add(ReverseGenerator.get(EmptyGenerator.get(width, 2)));
+        inputBIs.add(EmptyGenerator.get(width, 2));
+        // mispick
+        inputBIs.add(CutLayoutGenerator.get(AchuLayoutGenerator.get(width, 4), 2).get(0));
+        // Khali
+        inputBIs.add(EmptyGenerator.get(width, 10));
+
+        inputBIs.add(brocade);
+        inputBIs.add(PlainGenerator.get(width, 16));
+        inputBIs.add(EmptyGenerator.get(width, 960 - 16));
+
+        // mispick
+        inputBIs.add(ReverseGenerator.get(CutLayoutGenerator.get(AchuLayoutGenerator.get(width, 12), 6).get(0)));
+        // Khali
+        inputBIs.add(EmptyGenerator.get(width, 10));
+
+        int repeatWidth = 0;
+        int repeatHeight = 0;
+
+        for (BufferedImage bi : inputBIs) {
+            displayPixels(bi);
+            repeatWidth = bi.getWidth();
+            repeatHeight += bi.getHeight();
+        }
+        BufferedImage bi = ReverseGenerator.get(AddLayoutGenerator.get(repeatWidth, repeatHeight, inputBIs));
+        displayPixels(bi);
+        saveBMP(bi, String.format(out, repeatWidth, repeatHeight));
+    }
+
+    private static void displayPixels(BufferedImage fileOne) {
+        System.out.println(String.format("Width : %s, Height : %s", fileOne.getWidth(), fileOne.getHeight()));
+    }
+
+    private static void saveBMP(final BufferedImage bi, final String path) throws IOException {
+        ImageIO.write(bi, "bmp", new File(path));
+    }
+
+}
