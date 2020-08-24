6bf5394d9b1ee2764e2b5eaed525be4fdb360842
“varaprasadps-xinja“ <vara.prasad@xinja.com.au>
2020-08-24 14:07:38 +0530

    test

---
+++ src/main/java/com/varaprasadps/no2/AllDownConversion.java
@@ -0,0 +1,46 @@
+package com.varaprasadps.no2;
+
+import com.varaprasadps.image.*;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class AllDownConversion {
+
+    public static void main(final String[] args) throws IOException {
+
+        String out = "z-data/out/2/alldown.bmp";
+
+        List<BufferedImage> inputBIs = new LinkedList<>();
+
+        int width = 22;
+
+        inputBIs.add(EmptyGenerator.get(width, 2688));
+
+
+        int repeatWidth = 0;
+        int repeatHeight = 0;
+
+        for (BufferedImage bi : inputBIs) {
+            displayPixels(bi);
+            repeatWidth = bi.getWidth();
+            repeatHeight += bi.getHeight();
+        }
+        BufferedImage bi = LeftLayoutGenerator.get(HorizontalFlipGenerator.get(AddLayoutGenerator.get(repeatWidth, repeatHeight, inputBIs)));
+        displayPixels(bi);
+        saveBMP(bi, String.format(out, repeatWidth, repeatHeight));
+    }
+
+    private static void displayPixels(BufferedImage fileOne) {
+        System.out.println(String.format("Width : %s, Height : %s", fileOne.getWidth(), fileOne.getHeight()));
+    }
+
+    private static void saveBMP(final BufferedImage bi, final String path) throws IOException {
+        ImageIO.write(bi, "bmp", new File(path));
+    }
+
+}
