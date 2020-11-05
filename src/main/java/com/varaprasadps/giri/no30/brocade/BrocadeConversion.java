954a16538cc9bfc958b88a18cf11ebb831ee0fd7
varaprasadps-xinja <vara.prasad@xinja.com.au>
2020-11-05 07:21:44 +0530

    giri no 30

---
+++ src/main/java/com/varaprasadps/giri/no30/brocade/BrocadeConversion.java
@@ -0,0 +1,46 @@
+package com.varaprasadps.giri.no30.brocade;
+
+import com.varaprasadps.image.ColumnRepeatGenerator;
+import com.varaprasadps.image.LeftLayoutGenerator;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class BrocadeConversion {
+
+    public static void main(final String[] args) throws IOException {
+        JariConversion.main(null);
+        NimbuConversion.main(null);
+        RaniConversion.main(null);
+
+        String out = "z-giri/out/30/1brocade-%s-%s.bmp";
+
+        List<String> inputs = new LinkedList<>();
+        inputs.add("z-giri/out/30/1rani-900-1824.bmp");
+        inputs.add("z-giri/out/30/1jari-900-1824.bmp");
+        inputs.add("z-giri/out/30/1nimbu-900-1824.bmp");
+
+        List<BufferedImage> inputBIs = new LinkedList<>();
+        for (String input : inputs) {
+            inputBIs.add(ImageIO.read(new File(input)));
+        }
+
+        BufferedImage bi = LeftLayoutGenerator.get(ColumnRepeatGenerator.get(inputBIs));
+        displayPixels(bi);
+        saveBMP(bi, String.format(out, bi.getWidth(), bi.getHeight()));
+    }
+
+
+    private static void displayPixels(BufferedImage fileOne) {
+        System.out.println(String.format("Width : %s, Height : %s", fileOne.getWidth(), fileOne.getHeight()));
+    }
+
+    private static void saveBMP(final BufferedImage bi, final String path) throws IOException {
+        ImageIO.write(bi, "bmp", new File(path));
+    }
+
+}
