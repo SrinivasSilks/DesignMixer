d30e91bc17046bfb06ba9a1e19ba251907b19d87
“varaprasadps” <poppu.vara@gmail.com>
2021-01-19 22:29:02 +0530

    no 6 3 design

---
+++ src/main/java/com/varaprasadps/no6/a2020Nov/design3/brocade2/JariConversion.java
@@ -0,0 +1,68 @@
+package com.varaprasadps.no6.a2020Nov.design3.brocade2;
+
+import com.varaprasadps.image.*;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class JariConversion {
+
+    public static void main(final String[] args) throws IOException {
+
+        String out = "z-data/out/6/a2020nov/design3/2jari-%s-%s.bmp";
+
+        final BufferedImage body = HorizontalRepeatGenerator.get(1, ImageIO.read(new File("z-data/in/6/a2020nov/design3/brocade3/jari.bmp")));
+        BufferedImage meena = ReverseGenerator.get(HorizontalRepeatGenerator.get(5, ImageIO.read(new File("z-data/in/6/a2020nov/design3/j/meena.bmp"))));
+        BufferedImage jari = ReverseGenerator.get(HorizontalRepeatGenerator.get(5, ImageIO.read(new File("z-data/in/6/a2020nov/design3/j/jari.bmp"))));
+
+        int width = body.getWidth();
+
+        List<BufferedImage> inputBIs = new LinkedList<>();
+
+        inputBIs.add(EmptyGenerator.get(width, 32));
+
+        //locking
+        inputBIs.add(PlainGenerator.get(width, 4));
+        //mispick
+        inputBIs.add(CutLayoutGenerator.get(AchuLayoutGenerator.get(body.getWidth(), 4), 2).get(0));
+        //jamudu
+        inputBIs.add(EmptyGenerator.get(width, 10));
+
+        //jari
+        inputBIs.add(jari);
+        inputBIs.add(body);
+        //meena
+        inputBIs.add(meena);
+
+        //box
+        inputBIs.add(EmptyGenerator.get(width, 4));
+        //mispick
+        inputBIs.add(ReverseGenerator.get(CutLayoutGenerator.get(AchuLayoutGenerator.get(body.getWidth(), 4), 2).get(0)));
+        //jamudu
+        inputBIs.add(EmptyGenerator.get(width, 10));
+
+        int repeatWidth = 0;
+        int repeatHeight = 0;
+
+        for (BufferedImage bi : inputBIs) {
+            displayPixels(bi);
+            repeatWidth = bi.getWidth();
+            repeatHeight += bi.getHeight();
+        }
+        BufferedImage bi = ReverseGenerator.get(AddLayoutGenerator.get(repeatWidth, repeatHeight, inputBIs));
+        displayPixels(bi);
+        saveBMP(bi, String.format(out, repeatWidth, repeatHeight));
+    }
+
+    private static void displayPixels(BufferedImage fileOne) {
+        System.out.println(String.format("Width : %s, Height : %s", fileOne.getWidth(), fileOne.getHeight()));
+    }
+
+    private static void saveBMP(final BufferedImage bi, final String path) throws IOException {
+        ImageIO.write(bi, "bmp", new File(path));
+    }
+}
