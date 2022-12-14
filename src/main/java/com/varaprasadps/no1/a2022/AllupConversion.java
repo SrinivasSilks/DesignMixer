5f71c17aa96b50780f32d50fbf36bf37b9ab9180
Vara Prasad <poppu.vara@gmail.com>
2022-12-14 19:14:09 +0530

    vara

---
+++ src/main/java/com/varaprasadps/no1/a2022/AllupConversion.java
@@ -0,0 +1,43 @@
+package com.varaprasadps.no1.a2022;
+
+import com.varaprasadps.image.*;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class AllupConversion {
+
+    public static void main(final String[] args) throws IOException {
+
+        String out = "z-data/out/1/allup.bmp";
+
+        List<BufferedImage> inputBIs = new LinkedList<>();
+
+        inputBIs.add(BlackGenerator.get(2, 2688));
+
+        int repeatWidth = 0;
+        int repeatHeight = 0;
+
+        for (BufferedImage bi : inputBIs) {
+            displayPixels(bi);
+            repeatWidth = bi.getWidth();
+            repeatHeight += bi.getHeight();
+        }
+        BufferedImage bi = LeftLayoutGenerator.get(HorizontalFlipGenerator.get(AddLayoutGenerator.get(repeatWidth, repeatHeight, inputBIs)));
+        displayPixels(bi);
+        saveBMP(bi, String.format(out, repeatWidth, repeatHeight));
+    }
+
+    private static void displayPixels(BufferedImage fileOne) {
+        System.out.println(String.format("Width : %s, Height : %s", fileOne.getWidth(), fileOne.getHeight()));
+    }
+
+    private static void saveBMP(final BufferedImage bi, final String path) throws IOException {
+        ImageIO.write(bi, "bmp", new File(path));
+    }
+
+}
