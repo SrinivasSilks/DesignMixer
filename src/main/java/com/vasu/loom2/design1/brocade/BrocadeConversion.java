6421e3185e2bec670c76ccba5c32a45b4a143251
Vara Prasad <poppu.vara@gmail.com>
2023-01-04 11:04:45 +0530

    vara

--- src/main/java/com/vasu/loom2/design1/brocade/BrocadeConversion.java
+++ src/main/java/com/vasu/loom2/design1/brocade/BrocadeConversion.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
-import static com.vasu.loom1.ThreePlay.*;
+import static com.vasu.loom2.ThreePlay.*;
 
 
 public class BrocadeConversion {
--- src/main/java/com/vasu/loom2/design1/kanni/KanniConversion.java
+++ src/main/java/com/vasu/loom2/design1/kanni/KanniConversion.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
-import static com.vasu.loom1.TwoPlay.*;
+import static com.vasu.loom2.TwoPlay.*;
 import static java.lang.String.format;
 
 
