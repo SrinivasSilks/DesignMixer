e47d3d75e45ca16e938b2604c63ef26050e6cbc0
varaprasadps-jl <vara.prasad@johnlewis.co.uk>
2023-03-14 20:04:41 +0530

    vasu

---
+++ src/main/java/com/vasu/loom2/design8/pallu/PalluConversion.java
@@ -0,0 +1,57 @@
+package com.vasu.loom2.design8.pallu;
+
+import com.varaprasadps.image.CutLayoutGenerator;
+import com.varaprasadps.image.HorizontalRepeatGenerator;
+import com.varaprasadps.image.LeftLayoutGenerator;
+import com.varaprasadps.image.VerticalFlipGenerator;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import static com.vasu.loom2.TwoPlay.*;
+import static java.lang.String.format;
+
+
+public class PalluConversion {
+
+    public static BufferedImage get(BufferedImage right, BufferedImage left, BufferedImage rani, BufferedImage jari) throws IOException {
+        List<BufferedImage> brocades = new LinkedList<>();
+        brocades.add(rani(right, left, rani));
+        brocades.add(jari(getBorder(left), jari));
+        BufferedImage brocade = LeftLayoutGenerator.get(getBrocade(brocades));
+        displayPixels(brocade);
+
+        saveBMP(brocade, format("z-vasu/out/2/design8/pallu-%s-%s.bmp", brocade.getWidth(), brocade.getHeight()));
+        return brocade;
+    }
+
+    public static void main(final String[] args) throws IOException {
+        BufferedImage rightd = ImageIO.read(new File("z-vasu/in/2/design8/border/border.bmp"));
+        BufferedImage leftd = VerticalFlipGenerator.get(ImageIO.read(new File("z-vasu/in/2/design8/border/border.bmp")));
+
+        BufferedImage rightf = HorizontalRepeatGenerator.get(12, rightd);
+        BufferedImage right = CutLayoutGenerator.get(CutLayoutGenerator.get(rightf, 180, 1), 1920, 0);
+
+        BufferedImage leftf = HorizontalRepeatGenerator.get(12, leftd);
+        BufferedImage left = CutLayoutGenerator.get(CutLayoutGenerator.get(leftf, 180, 1), 1920, 0);
+
+        BufferedImage jari = ImageIO.read(new File("z-vasu/in/2/design8/pallu/pallu_jari.bmp"));
+        BufferedImage rani = ImageIO.read(new File("z-vasu/in/2/design8/pallu/pallu_rani.bmp"));
+        get(right, left, rani, jari);
+    }
+
+
+    private static void displayPixels(BufferedImage fileOne) {
+        System.out.println(format("Width : %s, Height : %s", fileOne.getWidth(), fileOne.getHeight()));
+    }
+
+    private static void saveBMP(final BufferedImage bi, final String path) throws IOException {
+        ImageIO.write(bi, "bmp", new File(path));
+    }
+
+
+}
