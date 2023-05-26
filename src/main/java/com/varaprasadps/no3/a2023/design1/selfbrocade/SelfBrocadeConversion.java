ae4dd8334df6238a0596403692e0c130b794d526
varaprasadps-jl <vara.prasad@johnlewis.co.uk>
2023-05-26 18:15:07 +0530

    test

---
+++ src/main/java/com/varaprasadps/no3/a2023/design1/selfbrocade/SelfBrocadeConversion.java
@@ -0,0 +1,48 @@
+package com.varaprasadps.no3.a2023.design1.selfbrocade;
+
+import com.varaprasadps.image.HorizontalRepeatGenerator;
+import com.varaprasadps.image.LeftLayoutGenerator;
+import com.varaprasadps.image.PlainGenerator;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import static com.varaprasadps.no3.a2022.TwoPlay.*;
+import static java.lang.String.format;
+
+
+public class SelfBrocadeConversion {
+
+    public static BufferedImage get(BufferedImage border, BufferedImage rani, BufferedImage jari) throws IOException {
+        List<BufferedImage> brocades = new LinkedList<>();
+        brocades.add(rani(border, rani));
+        brocades.add(jari(getBorder(border), jari));
+        BufferedImage brocade = LeftLayoutGenerator.get(getBrocade(brocades));
+        displayPixels(brocade);
+
+        saveBMP(brocade, format("z-data/out/3/a2023/design1/2brocade-%s-%s.bmp", brocade.getWidth(), brocade.getHeight()));
+        return brocade;
+    }
+
+    public static void main(final String[] args) throws IOException {
+        BufferedImage border = HorizontalRepeatGenerator.get(1, ImageIO.read(new File("z-data/in/3/a2023/design1/border/border.bmp")));
+        BufferedImage jari = HorizontalRepeatGenerator.get(4, ImageIO.read(new File("z-data/in/3/a2023/design1/brocade2/jari.bmp")));
+        BufferedImage rani = PlainGenerator.get(jari.getWidth(), 480);
+        get(border, rani, jari);
+    }
+
+
+    private static void displayPixels(BufferedImage fileOne) {
+        System.out.printf("Width : %s, Height : %s%n", fileOne.getWidth(), fileOne.getHeight());
+    }
+
+    private static void saveBMP(final BufferedImage bi, final String path) throws IOException {
+        ImageIO.write(bi, "bmp", new File(path));
+    }
+
+
+}
