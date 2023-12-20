ee508b8b559cc3402652492cb87556f6594c5451
Vara Prasad <vara.prasad@johnlewis.co.uk>
2023-12-20 19:23:56 +0530

    test

---
+++ src/main/java/com/varaprasadps/no3/a2023new/design2/selfbrocade4/SelfBrocadeConversion.java
@@ -0,0 +1,52 @@
+package com.varaprasadps.no3.a2023new.design2.selfbrocade4;
+
+import com.varaprasadps.image.HorizontalRepeatGenerator;
+import com.varaprasadps.image.LeftLayoutGenerator;
+import com.varaprasadps.image.VerticalFlipGenerator;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import static com.varaprasadps.no3.a2023new.design2.TwoPlay.*;
+import static java.lang.String.format;
+
+
+public class SelfBrocadeConversion {
+
+    public static BufferedImage get(BufferedImage left, BufferedImage right, BufferedImage rani, BufferedImage jari) throws IOException {
+        List<BufferedImage> brocades = new LinkedList<>();
+        brocades.add(rani(left, right, rani));
+        brocades.add(jari(jari));
+        BufferedImage brocade = LeftLayoutGenerator.get(getBrocade(brocades));
+        displayPixels(brocade);
+
+        saveBMP(brocade, format("d/3/out/design2/4self-brc-%s-%s.bmp", brocade.getWidth(), brocade.getHeight()));
+        return brocade;
+    }
+
+    public static void main(final String[] args) throws IOException {
+
+        BufferedImage jari = HorizontalRepeatGenerator.get(13, ImageIO.read(new File("d/3/in/design2/selfbrocade4/jari.bmp")));
+        BufferedImage rani = HorizontalRepeatGenerator.get(13, ImageIO.read(new File("d/3/in/design2/selfbrocade4/meena.bmp")));
+
+        BufferedImage right = HorizontalRepeatGenerator.get(10, ImageIO.read(new File("d/3/in/design2/border/right.bmp")));
+        BufferedImage left = VerticalFlipGenerator.get(HorizontalRepeatGenerator.get(10, ImageIO.read(new File("d/3/in/design2/border/left.bmp"))));
+
+        get(left, right, rani, jari);
+    }
+
+
+    private static void displayPixels(BufferedImage fileOne) {
+        System.out.println(format("Width : %s, Height : %s", fileOne.getWidth(), fileOne.getHeight()));
+    }
+
+    private static void saveBMP(final BufferedImage bi, final String path) throws IOException {
+        ImageIO.write(bi, "bmp", new File(path));
+    }
+
+
+}
