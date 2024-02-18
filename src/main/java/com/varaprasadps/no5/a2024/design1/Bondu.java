4c594fe25cde3385130d57fbff90d94b0cf322b5
Vara Prasad <vara.prasad@johnlewis.co.uk>
2024-02-18 19:18:58 +0530

    test

---
+++ src/main/java/com/varaprasadps/no5/a2024/design1/Bondu.java
@@ -0,0 +1,53 @@
+package com.varaprasadps.no5.a2024.design1;
+
+import com.varaprasadps.image.AddLayoutGenerator;
+import com.varaprasadps.image.CutLayoutGenerator;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Bondu {
+
+    public static void main(final String[] args) throws IOException {
+        String out = "d/5/out/2024/design1/kongu.bmp";
+
+        BufferedImage blouse = ImageIO.read(new File("d/5/out/2024/design1/blouse-1824-480.bmp"));
+        BufferedImage pallu = ImageIO.read(new File("d/5/out/2024/design1/pallu-1824-3200.bmp"));
+        BufferedImage brocade = ImageIO.read(new File("d/5/out/2024/design1/1kadiyalu-brocade-1824-3600.bmp"));
+
+        List<BufferedImage> inputBIs = new LinkedList<>();
+
+//        inputBIs.add(brocade);
+        inputBIs.add(CutLayoutGenerator.get(blouse, 120).get(0));
+//        inputBIs.add(pallu);
+//        inputBIs.add(CutLayoutGenerator.get(blouse, blouse.getHeight() - 40).get(1));
+//        inputBIs.add(CutLayoutGenerator.get(blouse, 120).get(0));
+
+
+        int repeatWidth = 0;
+        int repeatHeight = 0;
+
+        for (BufferedImage bi : inputBIs) {
+            displayPixels(bi);
+            repeatWidth = bi.getWidth();
+            repeatHeight += bi.getHeight();
+        }
+
+        BufferedImage bi = AddLayoutGenerator.get(repeatWidth, repeatHeight, inputBIs);
+        displayPixels(bi);
+        saveBMP(bi, String.format(out, repeatWidth, repeatHeight));
+    }
+
+    private static void displayPixels(BufferedImage fileOne) {
+        System.out.println(String.format("Width : %s, Height : %s", fileOne.getWidth(), fileOne.getHeight()));
+    }
+
+    static void saveBMP(final BufferedImage bi, final String path) throws IOException {
+        ImageIO.write(bi, "bmp", new File(path));
+    }
+
+}
