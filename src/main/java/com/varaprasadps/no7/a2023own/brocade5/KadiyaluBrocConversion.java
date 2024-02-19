9d9fb5e3cb675f719d1055399f1b59fc7f45625a
Vara Prasad <vara.prasad@johnlewis.co.uk>
2024-02-19 13:15:05 +0530

    test

---
+++ src/main/java/com/varaprasadps/no7/a2023own/brocade5/KadiyaluBrocConversion.java
@@ -0,0 +1,46 @@
+package com.varaprasadps.no7.a2023own.brocade5;
+
+import com.varaprasadps.image.ColumnRepeatGenerator;
+import com.varaprasadps.image.LeftLayoutGenerator;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class KadiyaluBrocConversion {
+
+    public static void main(final String[] args) throws IOException {
+        NimbuConversion.main(null);
+        JariConversion.main(null);
+        RaniConversion.main(null);
+        String out = "d/7/out/design1/5kadiyalu-broc-%s-%s.bmp";
+
+        List<String> inputs = new LinkedList<>();
+        inputs.add("d/7/out/design1/5kbroc-rani-2400-1824.bmp");
+        inputs.add("d/7/out/design1/5kbroc-jari-2400-1824.bmp");
+        inputs.add("d/7/out/design1/5kbroc-nimbu-2400-1824.bmp");
+
+        List<BufferedImage> inputBIs = new LinkedList<>();
+        for (String input : inputs) {
+            inputBIs.add(ImageIO.read(new File(input)));
+        }
+
+        BufferedImage bi = LeftLayoutGenerator.get(ColumnRepeatGenerator.get(inputBIs));
+        displayPixels(bi);
+        saveBMP(bi, String.format(out, bi.getWidth(), bi.getHeight()));
+    }
+
+
+    private static void displayPixels(BufferedImage fileOne) {
+        System.out.println(String.format("Width : %s, Height : %s", fileOne.getWidth(), fileOne.getHeight()));
+    }
+
+    private static void saveBMP(final BufferedImage bi, final String path) throws IOException {
+        ImageIO.write(bi, "bmp", new File(path));
+    }
+
+
+}
