34acb50c00686db15af2ad64d329c08696036944
Vara Prasad <vara.prasad@johnlewis.co.uk>
2024-08-14 00:08:12 +0530

    test

---
+++ src/main/java/com/varaprasadps/no2/a2024/design2/kadiyalubrocade3/KadiyaluBrocadeConversion.java
@@ -0,0 +1,48 @@
+package com.varaprasadps.no2.a2024.design2.kadiyalubrocade3;
+
+import com.varaprasadps.image.ColumnRepeatGenerator;
+import com.varaprasadps.image.HorizontalFlipGenerator;
+import com.varaprasadps.image.LeftLayoutGenerator;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class KadiyaluBrocadeConversion {
+
+    public static void main(final String[] args) throws IOException {
+
+        JariConversion.main(null);
+        NimbuConversion.main(null);
+        RaniConversion.main(null);
+
+        String out = "d/2/out/design2/3kadiyalu-brocade-%s-%s.bmp";
+
+        List<String> inputs = new LinkedList<>();
+        inputs.add("d/2/out/design2/3kbrc1-rani-1200-2688.bmp");
+        inputs.add("d/2/out/design2/3kbrc1-jari-1200-2688.bmp");
+        inputs.add("d/2/out/design2/3kbrc1-nimbu-1200-2688.bmp");
+        List<BufferedImage> inputBIs = new LinkedList<>();
+        for (String input : inputs) {
+            inputBIs.add(ImageIO.read(new File(input)));
+        }
+
+        BufferedImage bi = LeftLayoutGenerator.get(HorizontalFlipGenerator.get(ColumnRepeatGenerator.get(inputBIs)));
+        displayPixels(bi);
+        saveBMP(bi, String.format(out, bi.getWidth(), bi.getHeight()));
+    }
+
+
+    private static void displayPixels(BufferedImage fileOne) {
+        System.out.println(String.format("Width : %s, Height : %s", fileOne.getWidth(), fileOne.getHeight()));
+    }
+
+    private static void saveBMP(final BufferedImage bi, final String path) throws IOException {
+        ImageIO.write(bi, "bmp", new File(path));
+    }
+
+
+}
