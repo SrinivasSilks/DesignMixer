e82f4c2ac5682f976d2de15e2c89d057844c4403
Vara Prasad <vara.prasad@johnlewis.co.uk>
2024-10-30 06:48:56 +0530

    gg

---
+++ src/main/java/com/varaprasadps/no15/TwoPlay.java
@@ -0,0 +1,228 @@
+package com.varaprasadps.no15;
+
+import com.varaprasadps.image.*;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class TwoPlay {
+
+    public static void main(final String[] args) throws IOException {
+
+        String out = "d/15/out/design1/anni-%s-%s.bmp";
+
+        int width = 30;
+
+        BufferedImage rightSilk = PlainGenerator.get(width, 528);
+        BufferedImage rightJari = PlainGenerator.get(width, 528);
+        BufferedImage teegaSilk = PlainGenerator.get(width, 48);
+        BufferedImage teegaJari = PlainGenerator.get(width, 48);
+        BufferedImage banarasSilk = PlainGenerator.get(width, 64);
+        BufferedImage banarasJari = PlainGenerator.get(width, 64);
+        BufferedImage body = PlainGenerator.get(width, 480);
+
+        List<BufferedImage> inputBIs = new LinkedList<>();
+
+        inputBIs.add(EmptyGenerator.get(width, 32));
+
+        //jamudu
+        inputBIs.add(PlainGenerator.get(width, 4));
+        //locking
+        inputBIs.add(PlainGenerator.get(width, 4));
+        //mispick
+        inputBIs.add(CutLayoutGenerator.get(AchuLayoutGenerator.get(width, 4), 2).get(0));
+        //kadiyalu
+        inputBIs.add(EmptyGenerator.get(width, 2));
+        //kali
+        inputBIs.add(EmptyGenerator.get(width, 4));
+
+        //left
+        inputBIs.add(banarasJari);
+        inputBIs.add(banarasSilk);
+        inputBIs.add(teegaJari);
+        inputBIs.add(teegaSilk);
+        //allover
+        inputBIs.add(body);
+        //right
+        inputBIs.add(rightJari);
+        inputBIs.add(rightSilk);
+
+        //jamudu
+        inputBIs.add(PlainGenerator.get(width, 4));
+        //locking
+        inputBIs.add(PlainGenerator.get(width, 4));
+        //box
+        inputBIs.add(ReverseGenerator.get(EmptyGenerator.get(width, 2)));
+        inputBIs.add(EmptyGenerator.get(width, 2));
+        //mispick
+        inputBIs.add(ReverseGenerator.get(CutLayoutGenerator.get(AchuLayoutGenerator.get(width, 4), 2).get(0)));
+        //kadiyalu
+        inputBIs.add(EmptyGenerator.get(width, 1));
+        //wheel
+        inputBIs.add(EmptyGenerator.get(width, 1));
+
+        int repeatWidth = 0;
+        int repeatHeight = 0;
+
+        for (BufferedImage bi : inputBIs) {
+            displayPixels(bi);
+            repeatWidth = bi.getWidth();
+            repeatHeight += bi.getHeight();
+        }
+        BufferedImage bi = LeftLayoutGenerator.get(ReverseGenerator.get(AddLayoutGenerator.get(repeatWidth, repeatHeight, inputBIs)));
+        displayPixels(bi);
+        saveBMP(bi, String.format(out, repeatWidth, repeatHeight));
+    }
+
+
+    public static BufferedImage jari(
+            BufferedImage rightSilk, BufferedImage rightJari,
+            BufferedImage banarasSilk, BufferedImage banarasJari,
+            BufferedImage teegaSilk, BufferedImage teegaJari,
+            BufferedImage body
+    ) {
+
+        int width = rightJari.getWidth();
+        List<BufferedImage> inputBIs = new LinkedList<>();
+
+        inputBIs.add(EmptyGenerator.get(width, 32));
+
+        //jamudu
+        inputBIs.add(PlainGenerator.get(width, 4));
+        //locking
+        inputBIs.add(PlainGenerator.get(width, 4));
+        //mispick
+        inputBIs.add(CutLayoutGenerator.get(AchuLayoutGenerator.get(width, 4), 2).get(0));
+        //kadiyalu
+        inputBIs.add(EmptyGenerator.get(width, 2));
+        //kali
+        inputBIs.add(EmptyGenerator.get(width, 4));
+
+        //left
+        inputBIs.add(banarasJari);
+        inputBIs.add(banarasSilk);
+        inputBIs.add(teegaJari);
+        inputBIs.add(teegaSilk);
+        //allover
+        inputBIs.add(body);
+        //right
+        inputBIs.add(rightJari);
+        inputBIs.add(rightSilk);
+
+        //jamudu
+        inputBIs.add(PlainGenerator.get(width, 4));
+        //locking
+        inputBIs.add(PlainGenerator.get(width, 4));
+        //box
+        inputBIs.add(ReverseGenerator.get(EmptyGenerator.get(width, 2)));
+        inputBIs.add(EmptyGenerator.get(width, 2));
+        //mispick
+        inputBIs.add(ReverseGenerator.get(CutLayoutGenerator.get(AchuLayoutGenerator.get(width, 4), 2).get(0)));
+        //kadiyalu
+        inputBIs.add(EmptyGenerator.get(width, 1));
+        //wheel
+        inputBIs.add(EmptyGenerator.get(width, 1));
+
+        int repeatWidth = 0;
+        int repeatHeight = 0;
+
+        for (BufferedImage bi : inputBIs) {
+            displayPixels(bi);
+            repeatWidth = bi.getWidth();
+            repeatHeight += bi.getHeight();
+        }
+        return AddLayoutGenerator.get(repeatWidth, repeatHeight, inputBIs);
+    }
+
+    public static BufferedImage rani(
+            BufferedImage rightSilk, BufferedImage rightJari,
+            BufferedImage banarasSilk, BufferedImage banarasJari,
+            BufferedImage teegaSilk, BufferedImage teegaJari,
+            BufferedImage body
+    ) {
+
+        int width = rightJari.getWidth();
+        List<BufferedImage> inputBIs = new LinkedList<>();
+
+        inputBIs.add(EmptyGenerator.get(width, 32));
+
+        //jamudu
+        inputBIs.add(PlainGenerator.get(width, 4));
+        //locking
+        inputBIs.add(PlainGenerator.get(width, 4));
+        //mispick
+        inputBIs.add(CutLayoutGenerator.get(AchuLayoutGenerator.get(width, 4), 2).get(0));
+        //kadiyalu
+        inputBIs.add(EmptyGenerator.get(width, 2));
+        //kali
+        inputBIs.add(EmptyGenerator.get(width, 4));
+
+        //left
+        inputBIs.add(banarasJari);
+        inputBIs.add(banarasSilk);
+        inputBIs.add(teegaJari);
+        inputBIs.add(teegaSilk);
+        //allover
+        inputBIs.add(body);
+        //right
+        inputBIs.add(rightJari);
+        inputBIs.add(rightSilk);
+
+        //jamudu
+        inputBIs.add(PlainGenerator.get(width, 4));
+        //locking
+        inputBIs.add(PlainGenerator.get(width, 4));
+        //box
+        inputBIs.add(EmptyGenerator.get(width, 2));
+        inputBIs.add(ReverseGenerator.get(EmptyGenerator.get(width, 2)));
+        //mispick
+        inputBIs.add(ReverseGenerator.get(CutLayoutGenerator.get(AchuLayoutGenerator.get(width, 4), 2).get(0)));
+        //kadiyalu
+        inputBIs.add(EmptyGenerator.get(width, 1));
+        //wheel
+        inputBIs.add(EmptyGenerator.get(width, 1));
+
+        int repeatWidth = 0;
+        int repeatHeight = 0;
+
+        for (BufferedImage bi : inputBIs) {
+            displayPixels(bi);
+            repeatWidth = bi.getWidth();
+            repeatHeight += bi.getHeight();
+        }
+        return AddLayoutGenerator.get(repeatWidth, repeatHeight, inputBIs);
+    }
+
+    public static void displayPixels(BufferedImage fileOne) {
+        System.out.printf("Width : %s, Height : %s%n", fileOne.getWidth(), fileOne.getHeight());
+    }
+
+
+    public static BufferedImage empty(int width) {
+        return EmptyGenerator.get(width, 12);
+    }
+
+    public static BufferedImage getBorder(BufferedImage border) {
+        return EmptyGenerator.get(border.getWidth(), border.getHeight());
+    }
+
+    public static BufferedImage getBrocade(List<BufferedImage> inputs) {
+        for (int i = 0; i < inputs.size(); i++) {
+            System.out.printf("Brocade => Width : %s, Height : %s%n", inputs.get(0).getWidth(), inputs.get(0).getHeight());
+        }
+        BufferedImage bufferedImage = ColumnRepeatGenerator.get(inputs);
+        System.out.printf("final Brocade => Width : %s, Height : %s%n", bufferedImage.getWidth(), bufferedImage.getHeight());
+
+        return bufferedImage;
+    }
+
+    public static void saveBMP(final BufferedImage bi, final String path) throws IOException {
+        ImageIO.write(bi, "bmp", new File(path));
+    }
+
+
+}
